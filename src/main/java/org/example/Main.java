2b50deb368ba0f55d2656645b16cc356fe018fe0
Riyaz <riyazuddin.shaik@ukg.com>
2022-12-04 18:18:25 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C://Projects//Drivers//chromedriver.exe");
+        //Accept No of elements to be added.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter No of Elements to be added:");
+        int N = in.nextInt();
+        System.out.println("No of Elements to be added:" +N);
+        // Navigates to rge internet
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://the-internet.herokuapp.com");
+        //Add N number of Elements
+        driver.findElement(By.xpath("//a[@href='/add_remove_elements/']")).click();
+        for (int i = 0;i< N; i++){
+            driver.findElement(By.xpath("//button[text()=\"Add Element\"]")).click();
+        }
+        // Asserts that n of elements exists
+        List<WebElement> elmts = driver.findElements(By.xpath("//button[text()=\"Delete\"]"));
+        Assert.assertEquals(N,elmts.size());
+        driver.quit();
+    }
+}
