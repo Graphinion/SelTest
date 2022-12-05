effea103d2efb015558df3973ce72e079f0915f4
Riyaz <riyazuddin.shaik@ukg.com>
2022-12-04 19:20:57 -0500

    TO valida CI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,25 +12,28 @@ import org.testng.Assert;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C://Projects//Drivers//chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",
+            "C://Projects//Drivers//chromedriver.exe");
         //Accept No of elements to be added.
         Scanner in = new Scanner(System.in);
         System.out.println("Enter No of Elements to be added:");
         int noOfElements = in.nextInt();
-        System.out.println("No of Elements to be added:" +noOfElements);
+        System.out.println("No of Elements to be added:" + noOfElements);
         // Navigates to rge internet
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://the-internet.herokuapp.com");
+        WebDriver dr = new ChromeDriver();
+        dr.navigate().to("https://the-internet.herokuapp.com");
         //Add N number of Elements to page
-        driver.findElement(By.xpath("//a[@href='/add_remove_elements/']")).click();
-        for (int i = 0;i < noOfElements; i++){
-            driver.findElement(By.xpath("//button[text()=\"Add Element\"]")).click();
+        dr.findElement(
+            By.xpath("//a[@href='/add_remove_elements/']")).click();
+        for (int i = 0;i < noOfElements; i++) {
+            dr.findElement(
+                By.xpath("//button[text()=\"Add Element\"]")).click();
         }
         // Asserts that n of elements exists
         List<WebElement> elmts =
-            driver.findElements(By.xpath("//button[text()=\"Delete\"]"));
-        Assert.assertEquals(noOfElements , elmts.size());
-        driver.quit();
+            dr.findElements(By.xpath("//button[text()=\"Delete\"]"));
+        Assert.assertEquals(noOfElements, elmts.size());
+        dr.quit();
 
     }
 
