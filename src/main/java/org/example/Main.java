a2a8b19faa034e55d517eb0ce0d1b82ef6a0f3dd
Riyaz <riyazuddin.shaik@ukg.com>
2022-12-04 19:12:45 -0500

    TO valida CI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,19 +16,22 @@ public class Main {
         //Accept No of elements to be added.
         Scanner in = new Scanner(System.in);
         System.out.println("Enter No of Elements to be added:");
-        int N = in.nextInt();
-        System.out.println("No of Elements to be added:" +N);
+        int noOfElements = in.nextInt();
+        System.out.println("No of Elements to be added:" +noOfElements);
         // Navigates to rge internet
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://the-internet.herokuapp.com");
         //Add N number of Elements to page
         driver.findElement(By.xpath("//a[@href='/add_remove_elements/']")).click();
-        for (int i = 0;i< N; i++){
+        for (int i = 0;i < noOfElements; i++){
             driver.findElement(By.xpath("//button[text()=\"Add Element\"]")).click();
         }
         // Asserts that n of elements exists
-        List<WebElement> elmts = driver.findElements(By.xpath("//button[text()=\"Delete\"]"));
-        Assert.assertEquals(N,elmts.size());
+        List<WebElement> elmts =
+            driver.findElements(By.xpath("//button[text()=\"Delete\"]"));
+        Assert.assertEquals(noOfElements , elmts.size());
         driver.quit();
+
     }
+
 }
